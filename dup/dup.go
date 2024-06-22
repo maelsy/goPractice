41bc76e85755f5065bd2acbfc7ac8858018f1c84
shnyuk <shnyuk>
2024-06-22 19:11:57 +0300

    Working with files

--- dup/dup.go
+++ dup/dup.go
@@ -3,19 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
-	"bufio"
+	"strings"
 )
 
 func main() {
 	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		counts[input.Text()]++
-		fmt.Println(counts)
+	for _, filename := range os.Args[1:] {
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
-	}
+			fmt.Printf("%d\t%s\n", n , line)
+		} 
+    }
 }
